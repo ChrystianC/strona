223e4cb7292e817fd961ca1d5ffe39375c0e5978
ChrystianC <chrystian.cle.cracow@gmail.com>
2021-10-06 16:38:49 +0200

    repair svg in podroze

---
+++ podstrony/mercedes_light/js_m/zmienslajda.js
@@ -0,0 +1,35 @@
+var numer = Math.floor(Math.random()*3)+1;
+
+var timer1 = 0;
+var timer2 = 0;
+
+function ustawslajd(nrslajdu)
+{
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    numer = nrslajdu - 1;
+
+    schowaj();
+    setTimeout("zmienslajd()", 500);
+
+}
+
+function schowaj()
+{
+    $("#slider").fadeOut(500);
+}
+
+function zmienslajd()
+{
+    numer++; if (numer>3) numer=1;
+
+    var plik = "<img  src=\"slajdy/slajd" + numer + ".jpg\" style='100%' /> ";
+
+    document.getElementById("slider").innerHTML = plik;
+    $("#slider").fadeIn(500);
+
+    timer1 = setTimeout("zmienslajd()", 5000);
+    timer2 = setTimeout("schowaj()", 4500);
+
+}
+window.onload=zmienslajd;
